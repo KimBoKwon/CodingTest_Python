96d3926f4ff786263d5456499fde5e6efdf98b09
KimBoKwon <dkdn005@naver.com>
2023-02-09 18:33:46 +0900

    codingtest_python

---
+++ Lv1/삼총사.py
@@ -0,0 +1,22 @@
+# def solution(number):
+#     answer = 0
+#     for i in range(len(number)):
+#         first_num = number[i]
+#         for j in range(i+1, len(number)):
+#             second_num = number[j]
+#             for z in range(j+1, len(number)):
+#                 if(first_num + second_num + number[z] == 0):
+#                     answer += 1
+#     return answer
+
+
+from itertools import combinations
+
+def solution (number) :
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
+
+print(solution([-2, 3, 0, 2, -5]))
